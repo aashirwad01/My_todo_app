af16008ab8989ac94abfea87c3182e780d771899
Pankajkumarbasuri78 <pbasuri10078@gmail.com>
2022-02-03 23:45:42 +0530

    deleting todos

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,11 +1,39 @@
-import { FormControlLabel, FormGroup, ListItem } from '@material-ui/core';
+import { 
+    FormControlLabel, 
+    FormGroup, 
+    IconButton, 
+    ListItem, 
+    ListItemSecondaryAction, 
+    Typography 
+} from '@material-ui/core';
 import Checkbox from '@material-ui/core/Checkbox';
+
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { DELETE_TODO, TOGGLE_COMPLETED } from '../redux/actions';
+import DeleteIcon from '@material-ui/icons/Delete';
+
 
 export default function TodoItem({id,title,completed,important}) {
+    const dispatch = useDispatch();
 
     function toggleCheckBox(params){
         //Todo- Need to dispatch toggle action
+        dispatch({
+            type: TOGGLE_COMPLETED,
+            payload:{
+                id,
+            },
+        })
+    }
+
+    function handleDeleteClick() {
+        dispatch({
+            type: DELETE_TODO,
+            payload:{
+                id,
+            },
+        })
     }
 
 
@@ -22,11 +50,19 @@ export default function TodoItem({id,title,completed,important}) {
                   color="primary"
                 />
               }
-              label={title}
+              label={<Typography style={{
+                  textDecoration: completed && "line-through",
+              }}>
+                  {title}
+              </Typography>}
               >
-
               </FormControlLabel>
           </FormGroup>
+          <ListItemSecondaryAction>
+              <IconButton onClick={ handleDeleteClick } edge="end">
+                  <DeleteIcon />
+              </IconButton>
+          </ListItemSecondaryAction>
       </ListItem>
   );
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,10 @@
-import { ADD_TODO } from "./actions";
+import { ADD_TODO, DELETE_TODO, TOGGLE_COMPLETED } from "./actions";
 import initialState from "./initial-state";
 import shortid from "shortid";
 
 function reducer(state = initialState,action){
     switch (action.type) {
-        case ADD_TODO:
+        case ADD_TODO:{
             const {title} = action.payload;
 
             const newState = {
@@ -20,6 +20,34 @@ function reducer(state = initialState,action){
             }
             
             return newState;
+        }
+
+        case TOGGLE_COMPLETED:{
+            const newTodos = state.todos.map((todo) => {
+                if(todo.id === action.payload.id){
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+
+            const newState = {
+                ...state,
+                todos: newTodos,
+            }
+            return newState;
+        }
+        
+        case DELETE_TODO:{
+            const {id} = action.payload;
+
+            const newTodos = state.todos.filter((todo) => todo.id !== id);
+
+            const newState = {
+                ...state,
+                todos: newTodos,
+            }
+            return newState;
+        }
     
         default:
             return state;
